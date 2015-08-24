8208c5f75215a4cffe219b6b3fbd6292f522e000
Joe Barnes <barnesjd@gmail.com>
2015-08-24 16:00:26 -0500

    Correcting a vpc id reference and removing an not-yet existing security group

--- devops/aws-rds.tf
+++ devops/aws-rds.tf
@@ -14,15 +14,15 @@ resource "aws_db_subnet_group" "all_azs" {
 resource "aws_security_group" "lift_db_sg" {
   name = "tf-portal-user-db-rds-sg"
   description = "RDS security group for the User DB"
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${module.vpc.vpc_id}"
 
   ingress {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
     security_groups = [
-      "${module.vpc.bastion_accessible_sg_id}",
-      "${aws_security_group.lift_instance_sg.id}"
+      "${module.vpc.bastion_accessible_sg_id}"
+      #"${aws_security_group.lift_instance_sg.id}"
     ]
   }
 }
