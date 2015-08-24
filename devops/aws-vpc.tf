6528e54bbf07b02c01ea9b2a3bc3a8e30db979d8
Joe Barnes <barnesjd@gmail.com>
2015-08-24 11:12:36 -0500

    Made a dumb mistake and then copy-pasted it multiple times. Who needs a type checker anyway??

--- devops/aws-vpc.tf
+++ devops/aws-vpc.tf
@@ -3,9 +3,9 @@ module "vpc" {
   
   vpc_name = "lift-jetty-cluster"
   ec2_key_name = "${var.ec2_key_name}"
-  region = "{$var.region}"
-  zone_A = "{$var.zone_A}"
-  zone_B = "{$var.zone_B}"
-  nat_ami = "{$var.nat_ami}"
-  bastion_ami = "{$var.bastion_ami}"
+  region = "${var.region}"
+  zone_A = "${var.zone_A}"
+  zone_B = "${var.zone_B}"
+  nat_ami = "${var.nat_ami}"
+  bastion_ami = "${var.bastion_ami}"
 }
