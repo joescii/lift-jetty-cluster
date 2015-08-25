d25a179b8bcb51ec9605b7b24cb54dcc77dfc8e0
Joe Barnes <barnesjd@gmail.com>
2015-08-25 18:32:30 -0500

    Sudo all the things

--- devops/install.sh
+++ devops/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eux
 
-mkdir /opt/lift
+sudo mkdir /opt/lift
 cd /opt/lift
-gunzip -cdfv /tmp/app.tar.gz | tar -vx 
+sudo gunzip -cdfv /tmp/app.tar.gz | tar -vx 
 
 # Set up our user
-useradd -d /opt/lift lift
-echo '${user_password}' | passwd lift --stdin
-echo 'lift ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-chown -R lift /opt/lift
+sudo useradd -d /opt/lift lift
+sudo echo '${user_password}' | passwd lift --stdin
+sudo echo 'lift ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+sudo chown -R lift /opt/lift
 
 # Add the application to startup
-chmod 755 /opt/lift/universal/stage/bin/lift-jetty-cluster-aws
+sudo chmod 755 /opt/lift/universal/stage/bin/lift-jetty-cluster-aws
 sudo mv /tmp/lift-svc.sh /etc/init.d/lift
 sudo chmod 0750 /etc/init.d/lift
 
