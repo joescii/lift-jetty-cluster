43459d0dd63b16e866c8cef227bd9623b660ef7d
Joe Barnes <barnesjd@gmail.com>
2015-08-25 19:04:34 -0500

    Removing password and sudoer stuff since we really don't need that anyway

--- devops/install.sh
+++ devops/install.sh
@@ -6,8 +6,6 @@ sudo gunzip -cdfv /tmp/app.tar.gz | sudo tar -vx
 
 # Set up our user
 sudo useradd --home-dir /opt/lift lift
-sudo echo "lift:${user_password}" | chpasswd
-sudo echo 'lift ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 sudo chown -R lift /opt/lift
 
 # Add the application to startup
