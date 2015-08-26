295cea8984a05018e0a1e3e3f208b6d653787959
Joe Barnes <barnesjd@gmail.com>
2015-08-25 21:44:11 -0500

    Adding public key for SSH access

--- devops/install.sh
+++ devops/install.sh
@@ -6,6 +6,7 @@ sudo gunzip -cdfv /tmp/app.tar.gz | sudo tar -vx
 
 # Set up our user
 sudo useradd --home-dir /opt/lift lift
+sudo mv /tmp/authorized_keys /opt/lift/.ssh/authorized_keys
 sudo chown -R lift /opt/lift
 
 # Add the application to startup
