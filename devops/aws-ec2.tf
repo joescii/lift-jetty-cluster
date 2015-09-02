986d50aeaea782e85f97019908d94dd2d34d7392
Joe Barnes <barnesjd@gmail.com>
2015-09-01 21:14:27 -0500

    Overlooked the desired_capacity parameter of the ASG

--- devops/aws-ec2.tf
+++ devops/aws-ec2.tf
@@ -62,11 +62,11 @@ resource "aws_autoscaling_group" "lift_as" {
   availability_zones = ["${module.region.zone_A}", "${module.region.zone_B}"]
   vpc_zone_identifier = ["${module.vpc.zone_A_private_id}", "${module.vpc.zone_B_private_id}"]
   name = "lift-autoscaling-group-${var.timestamp}"
+  desired_capacity = 2
   max_size = 2
   min_size = 2
   health_check_grace_period = 300
   health_check_type = "ELB"
-  desired_capacity = 1
   force_delete = true
   launch_configuration = "${aws_launch_configuration.lift_as_conf.id}"
   load_balancers = ["${aws_elb.lift-elb.name}"]
