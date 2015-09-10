14912417493d69265bb02a1270a58d6b233b30ff
Joe Barnes <barnesjd@gmail.com>
2015-09-10 09:28:54 -0500

    Restoring everything

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -47,20 +47,20 @@ terraform remote config \
 # Retrieve modules from github
 terraform get
 
-#terraform apply \
-#  -var "access_key=${AWS_ACCESS_KEY_ID}" \
-#  -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
-#  -var "db_username=${DB_USERNAME}" \
-#  -var "db_password=${DB_PASSWORD}" \
-#  -var "timestamp=${timestamp}" \
-#  -var "ci_server_cidr_block=${cidrBlock}" 
-  
-# If ever needed...
-terraform destroy -force  \
+terraform apply \
   -var "access_key=${AWS_ACCESS_KEY_ID}" \
   -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
   -var "db_username=${DB_USERNAME}" \
   -var "db_password=${DB_PASSWORD}" \
-  -var "timestamp=${timestamp}" 
+  -var "timestamp=${timestamp}" \
+  -var "ci_server_cidr_block=${cidrBlock}" 
+  
+# If ever needed...
+#terraform destroy -force  \
+#  -var "access_key=${AWS_ACCESS_KEY_ID}" \
+#  -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
+#  -var "db_username=${DB_USERNAME}" \
+#  -var "db_password=${DB_PASSWORD}" \
+#  -var "timestamp=${timestamp}" 
 
 popd
