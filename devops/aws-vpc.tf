b470e3ba31a0604cf4c6b7af3b4dd3bd6fdff229
Joe Barnes <barnesjd@gmail.com>
2015-09-09 19:24:32 -0500

    Trying out my enhanced bastion

--- devops/aws-vpc.tf
+++ devops/aws-vpc.tf
@@ -7,6 +7,6 @@ module "vpc" {
   zone_A = "${module.region.zone_A}"
   zone_B = "${module.region.zone_B}"
   nat_ami = "${module.region.nat_ami}"
-  bastion_ami = "${module.region.bastion_ami}"
+  bastion_ami = "${module.region.enhanced_bastion_ami}"
   ci_server_cidr_block = "${var.ci_server_cidr_block}"
 }
